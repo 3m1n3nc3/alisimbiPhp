285b1877f9732b3f775f7e8458912f0dedef3807
3m1n3nc3 <mygames.ng@gmail.com>
2019-07-10 14:36:56 +0100

    Updated the tables and delivered my updates

--- templates/default/js/custom.js
+++ templates/default/js/custom.js
@@ -22,11 +22,11 @@ $(document).ready(function() {
 function errorHtml(message, response) {
   htmlX =
   '<div class="card m-2 text-center">'+
-  '<div class="card-header p-2">Server Response: </div>'+
-  '<div class="card-body p-2 text-info">'+
-  '<div class="card-text font-weight-bold text-danger">'+message+'</div>'
-  +response+
-  '</div>'+
+    '<div class="card-header p-2">Server Response: </div>'+
+    '<div class="card-body p-2 text-info">'+
+      '<div class="card-text font-weight-bold text-danger">'+message+'</div>'
+      +response+
+    '</div>'+
   '</div>';
   return htmlX;
 }
@@ -42,19 +42,20 @@ function connector(type, target) {
   if (type == 0) {
 
     /*For login*/
-    // var username = $('input[name="username"]').val();
-    // var password = $('input[name="password"]').val();
-    // var remember = $('input[name="remember"]').val();
+    var username = $('input[name="username"]').val();
+    var password = $('input[name="password"]').val();
+    var remember = $('input[name="remember"]').val();
 
-    let username = target.username.value;
-    let password = target.password.value;
-    let remember = target.remember.value;
-    let submit_b = target.submit.value;
+    // let username = target.username;
+    // let password = target.password;
+    // let remember = target.remember;
+    // let submit_b = target.submit;
+
 
     $(submit_b).attr('disabled', 'disabled');
     let url = siteUrl + "/connection/connector.php";
     let url_data = "username="+username+"&password="+password+"&remember="+remember+"&login=1";
-
+    console.log(url_data);
 
     $.ajax({
       type: "POST",
@@ -82,14 +83,14 @@ function connector(type, target) {
   }
   else if (type == 1) {
     /*For registration*/
-    var username = $('input[name="register-username"]').val();
-    var password = $('input[name="register-password"]').val();
-    var email = $('input[name="register-email"]').val();
-    var firstname = $('input[name="register-fname"]').val();
-    var lastname = $('input[name="register-lname"]').val();
-    var country = $('select[name="register-country"] option:selected').val();
-    var state = $('select[name="register-state"] option:selected').val();
-    var city = $('select[name="register-city"] option:selected').val();
+    var username = $('input[name="username"]').val();
+    var password = $('input[name="password"]').val();
+    var email = $('input[name="email"]').val();
+    var firstname = $('input[name="fname"]').val();
+    var lastname = $('input[name="lname"]').val();
+    var country = $('select[name="country"] option:selected').val();
+    var state = $('select[name="state"] option:selected').val();
+    var city = $('select[name="city"] option:selected').val();
 
     $.ajax({
       type: "POST",
@@ -98,16 +99,16 @@ function connector(type, target) {
       dataType:"json",
       cache: false,
       success: function(html) {
-        var response = html.message;
-        // $('#preloader').hide();
-        $(message).html(message);
+        var message = html.message;
+        $('#preloader').hide();
+        $('#reg_msg').html(message);
         if (html.status == 1) {
           window.top.location=html.header;
         }
       },
       error: function(xhr, status, error) {
         var errorMessage = 'An Error Occurred - ' + xhr.status + ': ' + xhr.statusText + '<br> ' + error;
-        $('message').html(errorHtml(errorMessage, xhr.responseText));
+        $('#reg_msg').html(errorHtml(errorMessage, xhr.responseText));
       }
     });
   }
@@ -115,14 +116,14 @@ function connector(type, target) {
 
 function fetch_state() {
   var country = document.getElementById("register-country");
-  var country_id = country.options[country.selectedIndex].id;
+  var country_id = country.options[country.selectedIndex].id;console.log(country_id);
   $.ajax({
     type: 'POST',
     url: siteUrl+'/connection/location.php',
     data: {country_id:country_id, type:2},
     success: function(html) {
-      $('#state').html(html);
-      $('#state').attr('onchange', 'fetch_city()');
+      $('#register-state').html(html);
+      $('#register-state').attr('onchange', 'fetch_city()');
     }
   })
 }
@@ -135,36 +136,7 @@ function fetch_city() {
     url: siteUrl+'/connection/location.php',
     data: {state_id:state_id, type:1},
     success: function(html) {
-      $('#city').html(html);
+      $('#register-city').html(html);
     }
   })
 }
-
-$('.cc-input').keyup(function (){
-  if(this.value.length === this.maxLength){
-    var $next = $(this).next('.cc-input');
-    console.log($next.length);
-    if($next.length){
-      $next.focus();
-    }else{
-      $(this).blur();
-    }
-  }
-});
-$('.cc-num').on("blur", function(){
-  $(this).attr('type', 'password');
-});
-$('.cc-group').on("focusin", function (){
-  $(this).css({
-    border: "1px solid green"
-  })
-});
-$('.cc-group').on("focusout", function (){
-  $(this).css({
-    border: "1px solid #d6d6d6"
-  })
-});
-$('.cc-num').on("focusin", function(){
-  $(".cc-num").attr('type', 'password');
-  $(this).attr('type', 'text');
-});
