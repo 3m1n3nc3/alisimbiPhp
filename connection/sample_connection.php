9db40764a1ffd2caff3c85e9c599fba46a386e1d
lil-armstrong <oebong1@gmail.com>
2019-08-01 16:38:00 +0100

    Merge remote-tracking branch 'origin/safety' into safety
    # Conflicts:
    #	.idea/workspace.xml
    #	controller/account.php
    #	index.php
    #	templates/default/html/training/add_questions.html
    #	templates/default/html/training/upload_video.html

--- connection/sample_connection.php
+++
@@ -1,5 +0,0 @@
-<?php 
-require_once(__DIR__ .'/../includes/autoload.php');
-
-$data = array('something' => '$main_content', 'loader' => '$loader', 'navigation' => '$navigation');
-echo json_encode($data, JSON_UNESCAPED_SLASHES); 
