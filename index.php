42bb360a77a861c9683fc6663cb7f12b43a193e9
3m1n3nc3 <mygames.ng@gmail.com>
2019-08-01 13:41:45 +0100

    Fix on redirection and test page

--- index.php
+++ index.php
@@ -9,9 +9,11 @@ if(isset($_GET['page']) && isset($action[$_GET['page']])) {
 
 if (isset($_GET['logout'])) {
     $out = $framework->sign_out();
-    $out == true ? header('Location: ' . cleanUrls($SETT['url'] . '/index.php?page=homepage')) : '';
+    header('Location: ' . cleanUrls($SETT['url'] . '/index.php?page=homepage'));
 } elseif ($user && $page_name == 'homepage') {
 	$framework->redirect('account&profile=home');
+} elseif ($user && $user['status'] == 0 && !isset($_GET['unverified'])) {
+	$framework->redirect('account&unverified=true');
 }
 
 require_once("controller/{$page_name}.php");
